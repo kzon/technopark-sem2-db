9c47d1bbdfe83fa51022e74cf8cbf7d7e749e786
anton <anton.cobzew@yandex.ru>
2019-12-24 15:32:20 +0300

    1500 rps

--- pkg/component/forum/repository/forum.go
+++ pkg/component/forum/repository/forum.go
@@ -38,31 +38,11 @@ func (r *Repository) CreateForum(title, slug, user string) (*model.Forum, error)
 	return r.GetForumByID(id)
 }
 
-func (r *Repository) FillForumPostsCount(forum int) (int, error) {
-	var count int
-	err := r.db.Get(
-		&count,
-		`update forum set posts=(select count(*) from post where forum=$1) where id=$1 and posts=0 returning posts`,
-		forum,
-	)
-	return count, err
-}
-
 func (r *Repository) GetForumUsers(forumSlug, since string, limit int, desc bool) (model.Users, error) {
-	forum, err := r.GetForumBySlug(forumSlug)
+	forum, err := r.GetForumSlug(forumSlug)
 	if err != nil {
 		return nil, err
 	}
-	needFill, err := r.needFillForumUsers(forum.Slug)
-	if err != nil {
-		return nil, err
-	}
-	if needFill {
-		if err := r.fillForumUsers(forum); err != nil {
-			return nil, err
-		}
-	}
-
 	sinceFilter := ""
 	if since != "" {
 		if desc {
@@ -76,7 +56,8 @@ func (r *Repository) GetForumUsers(forumSlug, since string, limit int, desc bool
 		limitExpr = fmt.Sprintf("limit %d", limit)
 	}
 	query := fmt.Sprintf(
-		`select "user".* from "user" join forum_user on nickname = forum_user.user
+		`select "user".* from "user"
+         		join forum_user on nickname = forum_user.user
 				where forum = $1 %s order by nickname %s %s`,
 		sinceFilter, r.getOrder(desc), limitExpr,
 	)
@@ -87,31 +68,4 @@ func (r *Repository) GetForumUsers(forumSlug, since string, limit int, desc bool
 		err = r.db.Select(&users, query, forum.Slug, since)
 	}
 	return users, err
-}
-
-func (r *Repository) needFillForumUsers(forum string) (bool, error) {
-	var result bool
-	err := r.db.Get(&result, `select not exists(select forum from forum_user where forum = $1)`, forum)
-	return result, err
-}
-
-func (r *Repository) fillForumUsers(forum *model.Forum) error {
-	nicknames := make([]string, 0)
-	err := r.db.Select(&nicknames,
-		`select nickname from "user" where (
-				exists(select id from post where author = "user".id and forum = $1) or
-				exists(select id from thread where author = nickname and forum = $2)
-			)`,
-		forum.ID, forum.Slug,
-	)
-	if err != nil {
-		return err
-	}
-	for _, nick := range nicknames {
-		_, err := r.db.Exec(`insert into forum_user (forum, "user") values ($1, $2)`, forum.Slug, nick)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
 }
